7cb0fe9adeb34245f2c4d748c1392b6ac38fd308
Michael Newman <coloradohokie@gmail.com>
2020-06-02 14:51:55 -0600

    add css styling to report controls

--- src/components/WeeklyReport/ReportControls/ReportControls.js
+++ src/components/WeeklyReport/ReportControls/ReportControls.js
@@ -1,16 +1,17 @@
 import React from 'react'
 import Form from 'react-bootstrap/Form'
+import classes from './ReportControls.module.css'
 import moment from 'moment'
 
 const reportControls = (props) => (
-    <div>
-        <Form.Group controlId="startDate">
-            <Form.Label>From</Form.Label>
+    <div className={classes.ReportControls}>
+        <Form.Group controlId="startDate" className={classes.DateControlGroup}>
+            <Form.Label style={{paddingRight: "5px"}}>From:</Form.Label>
             <Form.Control type="date" name="startDate" value={moment(props.startDate).format("YYYY-MM-DD")} onChange={props.handleDateChange} />
         </Form.Group>
 
-        <Form.Group controlId="endDate">
-            <Form.Label>To</Form.Label>
+        <Form.Group controlId="endDate" className={classes.DateControlGroup}>
+            <Form.Label style={{paddingRight: "5px"}}>To: </Form.Label>
             <Form.Control type="date" name="endDate" value={moment(props.endDate).format("YYYY-MM-DD")} onChange={props.handleDateChange} />
         </Form.Group>
     </div>
